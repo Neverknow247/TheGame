c59e90af1d80882a0163765e02fb6bb7140d30b7
Nathan Bell <nathanbell@Nathans-MacBook-Pro.local>
2020-12-04 10:34:11 -0600

    add base files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace theGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
